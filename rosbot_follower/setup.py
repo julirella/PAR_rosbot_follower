4613e5f3621eae37f0abd0963ad445a35df405ae
julirella <s4075758@student.rmit.edu.au>
2024-05-22 12:13:40 +1000

    implement tracker file and launch file, ready for find object 2d training

--- rosbot_follower/setup.py
+++ rosbot_follower/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'rosbot_follower'
 
@@ -7,8 +9,9 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
@@ -20,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            f"tracker = {package_name}.tracker:main",
         ],
     },
 )
