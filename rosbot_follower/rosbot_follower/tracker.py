493e36c1ce84a33c1265d746a01986b47c9a566b
Declan Baker <s3835358@student.rmit.edu.au>
2024-05-31 13:23:49 +1000

    Refactor follow, tracker

--- rosbot_follower/rosbot_follower/tracker.py
+++ rosbot_follower/rosbot_follower/tracker.py
@@ -4,27 +4,21 @@ import rclpy
 from rclpy.node import Node
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
-from rclpy.time import Time
-
 
 from std_msgs.msg import Float32MultiArray, Float64
-from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import PoseStamped, Pose
+from geometry_msgs.msg import PoseStamped
 import tf2_geometry_msgs
 
 import tf2_ros
-from turtle_tf2_py.turtle_tf2_broadcaster import quaternion_from_euler
 
 import numpy as np
 import cv2
-#listen to find object 2d to get
-#when something comes through, calculate distance using laser
-#calculate target position from that and publish
 
 CAMERA_FIELD_OF_VIEW = 120 #still not sure about this one
 IMAGE_WIDTH = 640
 
 class Tracker(Node):
+
     def __init__(self):
         super().__init__('mapper')
         self.reentrant_group_1 = ReentrantCallbackGroup()
@@ -33,23 +27,16 @@ class Tracker(Node):
         self.object_sub = self.create_subscription(Float32MultiArray, '/objects', self.object_callback,
             10, callback_group=self.reentrant_group_1)
         
-        self.laser_scan_sub = self.create_subscription(LaserScan, '/scan', self.laser_scan_callback,
-            10, callback_group=self.reentrant_group_1)
-        
-        #publishers
+        # Publishers
         self.robot_pose_pub = self.create_publisher(PoseStamped, '/follow/pose', 10)
         self.angular_offset_pub = self.create_publisher(Float64, '/follow/angular_gain', 10)
 
-        self.laser_scan = LaserScan()
-
-         # Transform listener
+        # Transform listener
         self.tf_buffer = tf2_ros.buffer.Buffer()
         self.tf_listener = tf2_ros.transform_listener.TransformListener(self.tf_buffer, self)
 
-        #flags
+        # Flags
         self.whole_target_pos = False
-
-
 
     def dst_points(self, detected_object_lst):
         object_width = detected_object_lst[1]
@@ -67,8 +54,10 @@ class Tracker(Node):
 
         src_pts = np.array([[top_left, top_right, bottom_left, bottom_right]], dtype='float32')
         # self.get_logger().info(f"homography matrix: {homography_matrix}\nsrc_pts: {src_pts}")
+        
         dst_pts = cv2.perspectiveTransform(src_pts, homography_matrix) 
         # self.get_logger().info(f"dst_pts: {dst_pts}")
+
         return dst_pts[0] #return a list of points rather than points being wrapped in two lists
     
     def get_centre_index(self, corner_points):
@@ -76,62 +65,6 @@ class Tracker(Node):
         y = int((corner_points[0,1] + corner_points[1,1] + corner_points[2,1] + corner_points[3,1]) // 4)
         # self.get_logger().info(f'centre indexes: x: {x}, y: {y}')
         return x, y
-    
-    def angle_from_image_index(self, x_index):
-        #centre offset should be positive if image pixel is to the left of image cenre
-        self.get_logger().info(f"x_index: {x_index}")
-        centre_offset = IMAGE_WIDTH / 2 - x_index
-        degrees_per_pixel = CAMERA_FIELD_OF_VIEW / IMAGE_WIDTH
-        angle = centre_offset * degrees_per_pixel
-        self.get_logger().info(f"angle (deg): {angle}")
-        return angle
-
-    def angle_from_image_index_rad(self, x_index):
-        return self.angle_from_image_index(x_index) * (np.pi / 180) 
-    
-    def get_distance_from_laser_scan(self, ranges, centre_x):
-        angle = int(self.angle_from_image_index(centre_x))
-        index = angle*2
-        self.get_logger().info(f"index: {index}, ranges len: {len(ranges)}")
-        
-        distance = ranges[index]
-
-        #if the laser cuts out for the required angle, just look to the side a little bit
-        oldIndex = index
-        while distance == np.inf:
-            index += 1
-            if index == len(ranges):
-                index = 0
-            if index == oldIndex: #to make sure we don't get stuck here forever
-                raise Exception("laser is full of infinities") #come up with better solution
-            distance = ranges[index]
-
-        return distance
-    
-    def generate_origin_pose(self):
-        pose = Pose()
-        pose.position.x = 0.0
-        pose.position.y = 0.0
-        pose.position.z = 0.0
-        pose.orientation.x = 0.0
-        pose.orientation.y = 0.0
-        pose.orientation.z = 0.0
-        pose.orientation.w = 1.0
-        return pose
-    
-    def transform_pose(self, src_frame, dest_frame, pose, time):
-        #not the most amazing implementation but usable
-        time = time - rclpy.duration.Duration(seconds=0.1)
-        for _ in range (1, 6):
-            try:
-                timeout = rclpy.duration.Duration(seconds=0.05)
-                transform = self.tf_buffer.lookup_transform(dest_frame, src_frame, time, timeout=timeout)
-                poseT = tf2_geometry_msgs.do_transform_pose(pose, transform)
-                return poseT
-            except tf2_ros.TransformException as ex:
-                time = time - rclpy.duration.Duration(seconds=1)
-
-        return self.generate_origin_pose()
     
     def calculate_pose_from_object(self, object_msg_data):
 
@@ -141,61 +74,14 @@ class Tracker(Node):
         centre_x, _ = self.get_centre_index(dst_pts)
         msg = Float64()
         msg.data = float(centre_x)
+        
         self.get_logger().info(f"tracker sending angular offset {msg}")
         self.angular_offset_pub.publish(msg)
-        # angle = self.angle_from_image_index_rad(centre_x)
         
-        # if self.whole_target_pos:
-        #     laser_image = self.laser_scan
-
-        #     object_distance = self.get_distance_from_laser_scan(laser_image.ranges, centre_x)
-            
-        #     #from now on laser frame coordinates, continuing as if laser frame and rgb camera frame were the same
-        #     laser_frame_x = object_distance * np.cos(angle)
-        #     laser_frame_y = object_distance * np.sin(angle)
-
-        #     laser_pose = self.generate_origin_pose()
-
-        #     # - because laser is the other way round from everything else
-        #     laser_pose.position.x = - laser_frame_x
-        #     laser_pose.position.y = - laser_frame_y
-
-        #     # self.get_logger().info(f"cam frame pose: x: {laser_frame_x}, y: {laser_frame_y}")
-
-        #     stamp = laser_image.header.stamp
-        #     pose = self.transform_pose('laser', 'map', laser_pose, Time.from_msg(stamp))
-        # else:
-        #     rotated_pose = self.generate_origin_pose()
-        #     orientation = quaternion_from_euler(0, 0, angle) #angle is around z axis so yaw
-        #     rotated_pose.orientation.x = orientation[0]
-        #     rotated_pose.orientation.y = orientation[1]
-        #     rotated_pose.orientation.z = orientation[2]
-        #     rotated_pose.orientation.w = orientation[3]
-
-        #     time = self.get_clock().now()
-        #     stamp = Time.to_msg(time)
-        #     pose = self.transform_pose('camera_color_frame', 'map', rotated_pose, time)
-
-        # pose_stamped = PoseStamped()
-        # pose_stamped.pose = pose
-        # pose_stamped.header.stamp = stamp
-        # pose_stamped.header.frame_id = 'map'
-        # return pose_stamped
-
-    def publish_pose(self, pose):
-        self.robot_pose_pub.publish(pose)
-    
-    def laser_scan_callback(self, scan_msg):
-        self.laser_scan = scan_msg
-
     def object_callback(self, object_msg):
         data = object_msg.data
         if len(data) > 0:
             self.calculate_pose_from_object(data)
-            # followed_robot_pose = self.calculate_pose_from_object(data)
-            # self.publish_pose(followed_robot_pose)
-            #instead of publishing the pose we could just call another function to navigate us here
-            
 
 def main(args=None):
     rclpy.init(args=args)
