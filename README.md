91f33b99a95076aae447a50d6948398272072197
Declan Baker <s3835358@student.rmit.edu.au>
2024-06-03 17:33:00 +1000

    Update lidar tracker logic

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ Start node:
 ros2 run rosbot_follower lidar_logger
 ```
 
-to get a reading open a second terminal:
+to prompt a reading open a second terminal:
 ```
 ros2 topic pub -1 /reading std_msgs/msg/Bool "data: true"
+```
+note that it may also be useful to compare these finding with the rviz display:
+```
+rviz2 -d $AIIL_CHECKOUT_DIR/humble_workspace/src/aiil_rosbot_demo/rviz/rosbot-default.rviz
 ```
--- rosbot_follower/rosbot_follower/lidar_track.py
+++ rosbot_follower/rosbot_follower/lidar_track.py
@@ -6,6 +6,8 @@ from sensor_msgs.msg import LaserScan
 from rclpy.qos import ReliabilityPolicy, QoSProfile
 from std_msgs.msg import Bool
 
+from math import sin, radians
+
 class LidarTrack(Node):
 
     def __init__(self):
@@ -41,23 +43,81 @@ class LidarTrack(Node):
         first_edge = None
         last_edge = None
         count = 0
-        range_thresh = 0.2
-        count_thresh = 4
+        range_thresh = 0.02
+        count_thresh = 3
 
         for index, range in enumerate(ranges):
-            if first_edge and abs(range - first_edge[1]) < range_thresh:
-                last_edge = (index, range)
-                print(f"{index/2} ADD")
-            elif range != float('inf') and not first_edge:
+            # find an edge
+            if range != float('inf') and not first_edge:
                 first_edge = (index, range)
-                print(f"{index/2} FIRST")    
-            elif count > count_thresh:
+                last_edge = (index, range)
+                print(f"{index/2}: {range} - FIRST")   
+            # if difference between edges is small enough
+            elif first_edge and self.within_threshold(last_edge[1], range):
+                last_edge = (index, range)
+                count = 0
+                print(f"{index/2}: {range} - ADD")
+            # If non candidate add to count
+            elif first_edge and count < count_thresh:
+                count += 1
+                print(f"{index/2}: {range} - COUNT: {count}") 
+            # if too many consecutive non candidates 
+            elif count >= count_thresh: 
+                print("RESET")
+                self.check_object(first_edge, last_edge, ranges)
                 first_edge = None
                 count = 0
             else:
-                count += 1
+                print(f"{index/2}: {range} - SKIP")
             #print(f"{index/2} is {range}")
+    
+    def check_object(self, first_edge, last_edge, ranges):
+        found = False
+        object_width = abs(first_edge[0] - last_edge[0])
+        sensible_edges = 3 <= object_width <= 60
+
+        # allow for greater error margin as distance increases
+        difference_threshold = 0.02 + 0.015*first_edge[1]
         
+        middle_index = round(max(first_edge[0], last_edge[0]) - abs(first_edge[0] - last_edge[0])/2)
+
+        #print(f"Given: {first_edge}, {last_edge}")
+        #print(f"Middle index: {middle_index}")
+        if ranges[middle_index] != float('inf'):
+            edge = max(first_edge[1], last_edge[1])
+            possibly_circular = 0.01 < edge - ranges[middle_index] < 0.13
+        #    print(f"Possible_1: {edge - ranges[middle_index]}") 
+        else:
+            possibly_circular = abs(first_edge[1] - last_edge[1]) < difference_threshold
+        #    print(f"Possible_2: {abs(first_edge[1] - last_edge[1])} < {difference_threshold}")
+        #print(f"Sensible_edges: {object_width}")
+
+        if sensible_edges and possibly_circular:
+            found = True
+            print(f"CHECK THIS OBJECT: ({first_edge[0]/2},{first_edge[1]}, ({last_edge[0]/2},{last_edge[1]})")
+            self.calculate_radius(first_edge, last_edge)
+        
+        return found
+
+    def calculate_radius(self, first_edge, last_edge):
+        angle = radians(abs(first_edge[0]/2 - last_edge[0]/2))
+        hypotenuse = max(first_edge[1], last_edge[1])
+        radius = sin(angle/2)*hypotenuse
+        print(f"angle: {abs(first_edge[0]/2 - last_edge[0]/2)} is {radians} radians")
+        print(f"hypotenuse: {hypotenuse}")
+        print(f"RADIUS: {radius}")
+        if 0.04 < radius < 0.1:
+            print(f"CANDIDATE")
+
+    def within_threshold(self, last_range, range):
+        within = False
+        threshold = last_range/35
+
+        if abs(last_range - range) < threshold:
+            within = True
+        
+        return within
+
 def main(args=None):
     # initialize the ROS communication
     rclpy.init(args=args)
